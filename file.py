e66193d12b821feac116d7fbd01226cd89fba232
--add <shreyanshmishra1202@gmail.com>
2025-05-14 12:36:52 +0530

    assignment of error and file handling

--- file.py
+++ file.py
@@ -7,7 +7,7 @@ file1=open('my_files.txt','r')
 f1=file1.read()             # reads whole file
 f2=file1.read(5)            # reads only x no. of character given in parenthesis() of read
 f3=file1.readline()         # reads a single line
-f4=file1.readlines()        # reads all ines but creates list of every lines
+f4=file1.readlines()        # reads all lines but creates list of every lines
 print(f1)
 print(f2)
 print(f3)
@@ -18,5 +18,5 @@ file1.close()
 #2
 with open('my_files.txt','r') as file1:
     # Statement (Dont use closing syntax)
-    f1=file1.read()     # we caqn apply other functions too as of 1st method
+    f1=file1.read()     # we can apply other functions too as of 1st method
     print(f1)
